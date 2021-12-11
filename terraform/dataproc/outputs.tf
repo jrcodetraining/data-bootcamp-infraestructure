73fa8188de1f67d9c9c49ff0eefa9dc724f8fd73
jrcodetraining <jr.code.training.21@gmail.com>
2021-12-10 22:36:46 -0700

    third commit

---
+++ terraform/dataproc/outputs.tf
@@ -0,0 +1,14 @@
+output "region" {
+  value       = var.region
+  description = "GCloud Region"
+}
+
+output "location" {
+  value       = var.location
+  description = "GCloud Region"
+}
+
+output "project_id" {
+  value       = var.project_id
+  description = "GCloud Project ID"
+}
